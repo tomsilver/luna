16b514dd97b8dee243a997f2afbf6b7dc0e748a6
tomsilver <tomssilver@gmail.com>
2016-01-14 17:00:01 -0500

    mongo port fix

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ require('./models/Questions');
 require('./models/Responses');
 require('./models/Games');
 
-var mongoStr = 'mongodb://tom:'+process.env.SECRET+'@ec2-52-23-169-221.compute-1.amazonaws.com:3000/luna';
+var mongoStr = 'mongodb://tom:'+process.env.SECRET+'@ec2-52-23-169-221.compute-1.amazonaws.com:27017/luna';
 
 mongoose.connect(mongoStr);
 
